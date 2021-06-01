92ba58285ab169b48696bd07b28bd05f806af8c2
NguyenNC <vietis.nguyennc>
2021-06-01 17:57:02 +0700

    ADD DATATABLE

--- affiliate-friend/src/components/content.js
+++ affiliate-friend/src/components/content.js
@@ -1,19 +1,42 @@
 import Panel from "./panel";
-import dataTable from "./data-table";
 import DataTable from "./data-table";
 
-function Content(){
-   return (
-       <>
-           <div className="row">
-               <div className="col-sm-12">
 
-               </div>
-           </div>
-           <Panel />
-           <DataTable />
-       </>
-   )
+export const getStaticProps = async () => {
+    const res = await fetch(`http://localhost:8003/api/items/`, {
+        method: "get",
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken"),
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+    })
+    console.log("Rest", res)
+    const data = await res.json();
+    console.log(data)
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {datatable: data}, // will be passed to the page component as props
+    }
+}
+
+function Content({datatable}) {
+    return (
+        <>
+            <div className="row">
+                <div className="col-sm-12">
+
+                </div>
+            </div>
+            <Panel/>
+            <DataTable data={datatable}/>
+        </>
+    )
 }
 
 
--- affiliate-friend/src/components/data-table.js
+++ affiliate-friend/src/components/data-table.js
@@ -1,9 +1,120 @@
-function DataTable(){
+import MaterialTable from "material-table";
+import {forwardRef} from 'react';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import './data-table.module.css'
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+};
+
+
+
+const useStyles = makeStyles({
+    caption: {
+        color: "green",
+        padding: 8,
+        border: "1px dashed grey",
+        fontSize: 20
+    },
+    toolbar: {
+        "& > p:nth-of-type(2)": {
+            fontSize: 20,
+            color: "red",
+            fontWeight: 600
+        }
+    }
+});
+
+const array = require("../../public/admin/static/data.json");
+let label = ["ASP名", "案件ID", "案件名"]
+
+function DataTable(props) {
+    const data = [
+        {name: "John", email: "john@gmail.com", age: 12, gender: "Male"},
+        {name: "Bren", email: "bren@gmail.com", age: 24, gender: "Male"},
+        {name: "Marry", email: "marry@gmail.com", age: 18, gender: "Female"},
+        {name: "Shohail", email: "shohail@gmail.com", age: 25, gender: "Male"},
+        {name: "Aseka", email: "aseka@gmail.com", age: 19, gender: "Female"},
+        {name: "Meuko", email: "meuko@gmail.com", age: 12, gender: "Female"},
+    ];
+    const columns = [
+        {
+            title: "NO",
+            field: 0,
+        },
+        {
+            title: "ASP名",
+            field: 1,
+        },
+        {
+            title: "案件ID",
+            field: 2,
+        },
+        {
+            title: "案件名",
+            field: 4,
+        },
+    ];
+    const classes = useStyles();
     return (
         <>
-            <h1>DATA TABLE</h1>
+            <div>
+                <MaterialTable
+                    title="表示期間 ダウンロード"
+                    data={props.data}
+                    columns={columns}
+                    options={{
+                        paging: false,
+                        search: false,
+                        exportButton: true,
+                        headerStyle: {
+                            fontSize: 20,
+                            backgroundColor: "rgba(102,102,102,1)",
+                            color: "white",
+                            textAlign: "center",
+                        },
+
+                    }}
+                    icons={tableIcons}
+                    classes={{
+                        toolbar: classes.toolbar,
+                        caption: classes.caption
+                    }}
+                />
+            </div>
         </>
     )
 }
 
-export default  DataTable;
+export default DataTable;
